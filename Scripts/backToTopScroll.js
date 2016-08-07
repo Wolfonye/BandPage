df4718fa33e34f7bc2923609372d0970fe0b2e0b
philipp-bous <wolf1bous@googlemail.com>
2016-08-07 20:23:02 +0200

    To-Top Button verbessert
    Das fade in war nciht smooth. Ist jetzt behoben. Ob die Lösung die beste ist, kann ich nciht mit Bestimmtheit sagen. Das alte Artefakt liegt unten in der JS-datei.

--- Scripts/backToTopScroll.js
+++ Scripts/backToTopScroll.js
@@ -1,3 +1,8 @@
+/*-------------ZULETZT GEÄNDERT AM 07.08.2016---------------------------------*/
+
+/*das Folgende ist ein Ansatz, der das Fading über das adden und removen von CSS-Klassen regelt.
+Etwas weniger clean als das obere, aber der Effekt ist smoother so; evtl wär es auch mit obigem Ansatz schöner
+gegangen, aber dafür fehlt mir noch das Wissen...ru*/
 $(document).ready(function(){
     var scrollDauer = 600,
         opVerringerungNach = 350,
@@ -5,7 +10,88 @@ $(document).ready(function(){
 	//Check to see if the window is top if not then display button
 	$(window).scroll(function(){
 		if ($(this).scrollTop() > einblendenNach) {
-			$('.headjumper').fadeIn();
+            $('.headjumper').addClass('isVisible');
+		} else {
+            $('.headjumper').removeClass('isVisible');
+            $('.headjumper').removeClass('lowOp');
+		}
+        if ($(this).scrollTop() > opVerringerungNach) {
+            $('.headjumper').addClass('lowOp');
+        }
+	});
+
+	//Click event to scroll to top
+	$('.headjumper').click(function(){
+		$('html, body').animate({scrollTop : $("#pagehead").offset().top
+    },scrollDauer);
+		return false;
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*ARTEFAKT
+
+$(document).ready(function(){
+    var scrollDauer = 600,
+        opVerringerungNach = 350,
+        einblendenNach = 150;
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > einblendenNach) {
+            $('.headjumper').fadeIn();
             $('.headjumper').addClass('isVisible');
 		} else {
 			$('.headjumper').fadeOut();
@@ -24,3 +110,4 @@ $(document).ready(function(){
 	});
 
 });
+*/
